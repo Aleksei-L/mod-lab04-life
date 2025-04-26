f330941b8f6ab301b8e72b17fa402bd624fc6a7f
Aleksei Lobanov <aleksei.lobanov@telmast.com>
2025-04-27 01:32:48 +0300

    wesfg

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,4 +1,5 @@
-﻿using cli_life;
+﻿using System.Text.Json;
+using cli_life;
 
 namespace NET
 {
@@ -77,7 +78,7 @@ namespace NET
         public void SettingsDeserialization_Test()
         {
             var json = "{\"width\":10,\"height\":10,\"liveDensity\":0.5}";
-            var settings = System.Text.Json.JsonSerializer.Deserialize<Settings>(json);
+            var settings = JsonSerializer.Deserialize<Settings>(json);
             Assert.AreEqual(10, settings.width);
             Assert.AreEqual(0.5, settings.liveDensity);
         }
@@ -129,9 +130,72 @@ namespace NET
         public void CountLiveCells_AfterAdvance_Test()
         {
             var board = new Board(10, 10, 1, 1.0);
-board.advance();
+            board.advance();
             int liveCells = board.countLiveCells();
             Assert.IsTrue(liveCells >= 0);
+        }
+
+        [TestMethod]
+        public void BoardInitialization_AllCellsCreated_Test()
+        {
+            var board = new Board(10, 10, 1, 0.0);
+            Assert.AreEqual(10, board.columns);
+            Assert.AreEqual(10, board.rows);
+        }
+
+        [TestMethod]
+        public void BoardRandomize_CorrectDensityApproximate_Test()
+        {
+            var board = new Board(100, 100, 1, 0.5);
+            int liveCells = board.countLiveCells();
+            int totalCells = board.columns * board.rows;
+            double density = (double)liveCells / totalCells;
+
+            Assert.IsTrue(density > 0.4 && density < 0.6, $"Actual density: {density}");
+        }
+
+        [TestMethod]
+        public void BoardAdvance_StillLifeRemains_Test()
+        {
+            var board = new Board(4, 4, 1, 0.0);
+            // ставим блок
+            board.cells[1, 1].isAlive = true;
+            board.cells[1, 2].isAlive = true;
+            board.cells[2, 1].isAlive = true;
+            board.cells[2, 2].isAlive = true;
+
+            board.advance();
+
+            Assert.IsTrue(board.cells[1, 1].isAlive);
+            Assert.IsTrue(board.cells[1, 2].isAlive);
+            Assert.IsTrue(board.cells[2, 1].isAlive);
+            Assert.IsTrue(board.cells[2, 2].isAlive);
+        }
+
+        [TestMethod]
+        public void CountLiveCells_ReturnsCorrectCount_Test()
+        {
+            var board = new Board(3, 3, 1, 0.0);
+            board.cells[0, 0].isAlive = true;
+            board.cells[1, 1].isAlive = true;
+            board.cells[2, 2].isAlive = true;
+
+            Assert.AreEqual(3, board.countLiveCells());
+        }
+
+        [TestMethod]
+        public void SaveAndLoadState_PreservesCellStates_Test()
+        {
+            var board = new Board(3, 3, 1, 0.0);
+            board.cells[1, 1].isAlive = true;
+            string path = "../../../test_save.txt";
+            board.saveState(path);
+
+            var loaded = Board.loadState(path);
+            Assert.IsTrue(loaded.cells[1, 1].isAlive);
+            Assert.IsFalse(loaded.cells[0, 0].isAlive);
+
+            File.Delete(path); // очищаем тестовый файл
         }
     }
 }
